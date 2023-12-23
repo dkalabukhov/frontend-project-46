c898dab5bd9ac1d57e54bcdc165d114e4d9b33df
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-12-23 20:44:05 +0300

    Fourth Step Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import { readFileSync } from 'node:fs';
 import path from 'node:path';
 
@@ -12,5 +13,36 @@ export default function showDiff(filepath1, filepath2) {
   const obj1 = JSON.parse(readFileSync(path1, 'utf-8'))
   const obj2 = JSON.parse(readFileSync(path2, 'utf-8'))
 
-  return [obj1, obj2];
+  const data1Keys = _.keys(obj1);
+  const data2Keys = _.keys(obj2);
+
+  const keys = _.sortBy(_.union(data1Keys, data2Keys));
+
+  const indent = '  ';
+
+  function stylish(lines) {
+    return [
+      '{',
+      ...lines,
+      '}'
+    ].join('\n');
+  }
+
+  const lines = keys.map((key) => {
+    if (!_.has(obj1, key)) {
+      return `${indent}+ ${key}: ${obj2[key]}`;
+    }
+
+    if (!_.has(obj2, key)) {
+      return `${indent}- ${key}: ${obj1[key]}`;
+    }
+
+    if (_.isEqual(obj1[key], obj2[key])) {
+      return `${indent}  ${key}: ${obj1[key]}`;
+    }
+
+    return `${indent}- ${key}: ${obj1[key]}\n${indent}+ ${key}: ${obj2[key]}`;
+  })
+
+  return stylish(lines);
 }
